5f83b4849ca1df5e0d2d7f6cab29cfe6ba45433a
Thevindu Hansara <75308046+ThevinduHansara@users.noreply.github.com>
2021-08-07 22:10:53 +0530

    Update onetextlogos.js

--- plugins/onetextlogos.js
+++ plugins/onetextlogos.js
@@ -3,7 +3,7 @@ const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys'
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');
-const need = "```YOU MUST ENTER WORDS!```"
+const NEED_WORD = "```YOU MUST ENTER WORDS!```"
 
 if (Config.WORKTYPE == 'public') {
 
