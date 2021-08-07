d29c16023e53f244fe02e04ef7ba4d9656a9ce3e
twiiceuif <88446358+twiiceuif@users.noreply.github.com>
2021-08-07 20:26:23 +0530

    Update lpack.js

--- plugins/lpack.js
+++ plugins/lpack.js
@@ -1,11 +1,9 @@
 const Asena = require('../events');
-const Config = require('../config');
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
-
-// LIST
-const NEED_WORD = "```YOU MUST ENTER WORDS!```"
+const Config = require('../config');
+const need = "```YOU MUST ENTER WORDS!```"
 
 if (Config.WORKTYPE == 'public') {
 
